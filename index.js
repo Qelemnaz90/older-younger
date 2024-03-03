dc33b5d4f2b910a5d59855fde07637dd6976e168
Qelemnaz90 <enazy1990@gmail.com>
2024-03-03 14:39:28 +0400

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+var age = prompt ("Enter your age: ")
+var me = 33 
+var a = me - age
+var b = age - me
+//if (age >33 ) { alert("you are older than me")}
+//else alert("you are younger than me" )
+
+if ( me < age ) {
+    alert ("you are older than me" + a + "years")
+} else if( me > age ) {alert
+    ("you are younger than me" + b + "years")}
